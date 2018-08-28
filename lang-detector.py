ae0de8c80ff5e35092049e8e042611130163edaf
Ryota Yamada <roy29fuku@gmail.com>
2018-08-28 13:22:36 +0900

    modify lang-detector.py ファイルを読み込んで国籍取得

--- lang-detector.py
+++ lang-detector.py
@@ -1,4 +1,5 @@
 import os
+import json
 from google.cloud import translate
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = './google-credentials.json'
@@ -7,16 +8,32 @@ os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = './google-credentials.json'
 def detect_language(text):
     """Detects the text's language."""
     translate_client = translate.Client()
-
-    # Text can also be a sequence of strings, in which case this method
-    # will return a sequence of results for each text.
     result = translate_client.detect_language(text)
 
-    print('Text: {}'.format(text))
-    print('Confidence: {}'.format(result['confidence']))
-    print('Language: {}'.format(result['language']))
+    # print('Text: {}'.format(text))
+    # print('Confidence: {}'.format(result['confidence']))
+    # print('Language: {}'.format(result['language']))
+
+    return result['language']
+
+
+def detect_language_from_json(file_path):
+    with open(file_path) as f:
+        data = json.load(f)
+
+    for d in data:
+        authors = d['authors']
+        coutries = list(map(detect_language, authors))
+        d['country'] = coutries
+
+    return data
 
 
 if __name__ == '__main__':
-    name = 'Ryota Yamada'
-    detect_language(name)
+    input_file_path = 'data/test.json'
+    output_file_path = 'data/test_detected.json'
+
+    data = detect_language_from_json(input_file_path)
+
+    with open(output_file_path, 'w') as f:
+        json.dump(data, f, indent=4)
