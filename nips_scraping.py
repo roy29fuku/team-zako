226734751ff8abe002e1d6f08a6e8581121d2e21
miura <miura.biwa@ist.osaka-u.ac.jp>
2018-08-30 15:17:42 +0900

    modified nips_scraping.py

--- nips_scraping.py
+++ nips_scraping.py
@@ -2,7 +2,10 @@ import json
 import requests
 import lxml.html
 from tqdm import tqdm
+import sys
 
+#NIPS第i回
+# => [{title, authors[], pdf_url},...]
 def get_nips(i):
     data = []
     url = 'https://papers.nips.cc/book/advances-in-neural-information-processing-systems-{}-{}'.format(i,1987+i)
@@ -11,29 +14,33 @@ def get_nips(i):
     html = lxml.html.fromstring(res.text)
     for li in tqdm(html.xpath('//div[@class="main wrapper clearfix"]/ul/li')):
         a = li.xpath('a')
+        pdf_url = url_base + a[0].attrib['href'] + '.pdf'
         #print('title: ' + a[0].text)
         #print('authors: ' + ', '.join([author.text for author in a[1:]]))
-        pdf_url = url_base + a[0].attrib['href'] + '.pdf'
         #print('url: ' + pdf_url)
         '''
+	#pdf_urlが生きてるか雑に確認しようとした
         stc = requests.get(pdf_url).status_code
         if stc >= 300:
-            print('!!! responce: {}, title: {}'.format(stc, a[0].text))
+            sys.stderro.write('!!! responce: {}, title: {}'.format(stc, a[0].text))
             continue
         '''
         data.append({'title':a[0].text, 'authors':[author.text for author in a[1:]], 'pdf': pdf_url})
     return data
 
-
-
+#python nips_scraping.py
+#python nips_scraping.py <num> <output_file>
 if __name__ == '__main__':
-    '''
-    with open('nips.json', mode='r') as f:
-        data = json.loads(f.read())
-    '''
-    #データの更新
-    data = []
-    for i in tqdm(range(1,31)):
-        data.extend(get_nips(i))
-    with open('nips.json', mode='w') as f:
-        json.dump(data, f, ensure_ascii=False, indent=2, sort_keys=True, separators=(',', ': '))
+    if len(sys.argv) > 2:
+        data = get_nips(int(sys.argv[1]))
+        if len(data) == 0:
+            sys.stderr.write('num => [1,2,...,30]')
+            exit()
+        with open(sys.argv[2], mode='w') as f:
+            json.dump(data, f, ensure_ascii=False, indent=2, sort_keys=True, separators=(',', ': '))
+    else:
+        data = []
+        for i in tqdm(range(1,31)):
+            data.extend(get_nips(i))
+        with open('nips.json', mode='w') as f:
+            json.dump(data, f, ensure_ascii=False, indent=2, sort_keys=True, separators=(',', ': '))
